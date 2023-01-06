beee8627ed54744336ba9736af814702c7c79c05
kaspalatet <70353232+kaspalatet@users.noreply.github.com>
2023-01-06 20:16:12 +0000

    new file:   .gitignore 	new file:   .npmrc 	new file:   .prettierignore 	new file:   .prettierrc 	modified:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   playwright.config.ts 	new file:   src/app.css 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/index.test.ts 	new file:   src/routes/+layout.svelte 	new file:   src/routes/+page.svelte 	new file:   src/routes/about/+page.svelte 	new file:   static/favicon.png 	new file:   svelte.config.js 	new file:   tests/test.ts 	new file:   tsconfig.json 	new file:   vite.config.ts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import '../app.css'
+</script>
+
+<nav class="navMenu">
+    <a href="/">Jour</a>
+    <a href="/about">Nuit</a>
+    <div class="dot"></div>
+  </nav>
+  
+  <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Jour</h1>
+<p><a href="/about">La nuit c'est là</a></p>
